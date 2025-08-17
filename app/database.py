067ede2b004d07334530896f90680762e6477167
nasser <nasraddinejj@gmail.com>
2025-08-17 03:45:14 +0200

    ADD/ chore(db)

---
+++ app/database.py
@@ -0,0 +1,16 @@
+﻿from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# SQLite for simplicity; we can make this env-driven later
+DATABASE_URL = "sqlite:///./app.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
